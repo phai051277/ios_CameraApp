916c4880cc43d1ff5dc17f4f2f25da230d27097f
phai0512 <phai0512@gmail.com>
2017-12-04 15:43:07 +0900

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by 小池基文 on 2017/12/04.
+//  Copyright © 2017年 小池基文. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
